ce6ab48480b5596a2aaebad0744d1e3f6ef2b4de
h3d-anhntl <anhntl@house3d.net>
2024-05-31 10:52:40 +0700

    luu user_id vao cookie

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -27,8 +27,32 @@ const Login = () => {
     signIn("credentials", {email, password})
   }
 
+  // Hàm tạo cookie
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  }
+
+  // Hàm lấy cookie
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
   const handleLoginWithGoogle = async (e) => {
-    sendGTMEvent({ event: 'login', user_id: (Math.floor(Math.random() * 10000) + 1) + 'user' })
+    
+    setCookie('user_id', (Math.floor(Math.random() * 10000) + 1) + 'user', 30);
+    sendGTMEvent({ event: 'login', user_id: getCookie('user_id')})
     signIn("google");
     
   }
